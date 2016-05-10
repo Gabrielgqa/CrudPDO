e871f0dce27b5d34e45c7b1adc622e7b488a1f4f
Gabrielgqa <gabrielgqa@live.com>
2016-05-10 08:15:25 -0300

    CRUD delete

--- controllers/delete_user.php
+++ controllers/delete_user.php
@@ -2,9 +2,8 @@
 	require_once('../config/config.php');
 	require_once('../models/User.php');
 
-	if (isset($_POST)) {
-		$user = new User($_POST);
-		$user->insert($pdo);
-		header('Location: ../index.php');
+	if (isset($_GET['id'])) {
+		if(User::delete($_GET['id'], $pdo))
+			header('Location: ../index.php');
 	}
 ?>
