6b6a679d1d4cda0e781390d3152b78b88f2a52d7
GabrielGQA <gabrielgomesqa@dcc.ufba.br>
2018-05-14 16:38:04 -0300

    entidade projetos

--- controllers/ProjetoController.php
+++ controllers/ProjetoController.php
@@ -2,15 +2,17 @@
 	require_once('../config/config.php');
 	require_once('../models/Projeto.php');
     $action = $_REQUEST['action'];
+    session_start();
 
     switch ($action) {
         case 'create':
             if (isset($_REQUEST)) {
+                $_REQUEST['id_usuario'] = $_SESSION['id'];
                 $user = new Projeto($_REQUEST);
                 if ($user->insert($pdo)) {
                     session_start();
                     $_SESSION['message'] = "Projeto criado com sucesso!";
-                    header('Location: ../views/setor/listar.php');
+                    header('Location: ../views/projeto/listar.php');
                 }
             }
         break;
@@ -21,7 +23,7 @@
                 if ($user->update($_REQUEST['id'], $pdo)) {
                     session_start();
                     $_SESSION['message'] = "Projeto atualizado com sucesso!";
-                    header('Location: ../views/setor/listar.php');
+                    header('Location: ../views/projeto/listar.php');
                 }
             }
         break;
@@ -31,12 +33,12 @@
                 if(Projeto::delete($_GET['id'], $pdo)) {
                     session_start();
                     $_SESSION['message'] = "Projeto deletado com sucesso!";
-                    header('Location: ../views/setor/listar.php');
+                    header('Location: ../views/projeto/listar.php');
                 }
             }
         break;
         default:
-            header('Location: ../views/setor/listar.php');
+            header('Location: ../views/projeto/listar.php');
         break;
     }
 ?>
--- views/projeto/criar.php
+++ views/projeto/criar.php
@@ -6,51 +6,29 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-lg-12">
-          <h3 class="page-header">Criar projeto</h3>
+          <h3 class="page-header">Criar Projeto</h3>
           <form method="POST" action="../../controllers/ProjetoController.php">
-              <div class="form-group">
-                <label for="nome">Nome</label>
-                <input type="text" class="form-control" id="nome" name="nome" required>
-              </div>
-              <div class="form-group">
-                <label for="descricao">Descrição</label>
-                <input type="text" class="form-control" id="descricao" name="descricao" required>
-              </div>
-              <div class="form-group">
-                <label for="data_ini">Data Inicial</label>
-                <input type="text" class="form-control" id="data_ini" name="data_ini" required>
-              </div>
-              <div class="form-group">
-                <label for="data_previsto">Data Final Prevista</label>
-                <input type="text" class="form-control" id="data_previsto" name="data_previsto" required>
-              </div>
-              <input type="hidden" class="form-control" id="action" name="action" value="create">
-              <input type="submit"  class="btn btn-default" value="Criar" style="float: right;">
+            <div class="form-group">
+              <label for="nome">Nome</label>
+              <input type="text" class="form-control" id="nome" name="nome" required>
+            </div>
+            <div class="form-group">
+              <label for="descricao">Descrição</label>
+              <input type="text" class="form-control" id="descricao" name="descricao" required>
+            </div>
+            <div class="form-group">
+              <label for="data_ini">Data Inicial</label>
+              <input type="date" class="form-control" id="data_ini" name="data_ini" required>
+            </div>
+            <div class="form-group">
+              <label for="data_previsto">Data Final Prevista</label>
+              <input type="date" class="form-control" id="data_previsto" name="data_previsto" required>
+            </div>
+            <div class="form-group">
+                <input type="hidden" class="form-control" id="action" name="action" value="create">
+                <input type="submit"  class="btn btn-default" value="Criar" style="float: right;">
+            </div>
           </form>
         </div>
       </div>
     </div>
-  </div>
-  <?php require_once('../../inc/scripts.php'); ?>
-</body>
-</html>
-
-<?php
-    require_once('../../config/config.php');
-    require_once('../../models/Setor.php');
-
-    $setores = Setor::selectAll($pdo);
- ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <title>Sistema Tarefas</title>
-  <link rel="stylesheet" href="../../assets/bootstrap.min.css">
-</head>
-<body>
-  <div class="container" style="padding-top: 40px;">
-
-  </div>
-</body>
-</html>
